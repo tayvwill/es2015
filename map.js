3d223f81c26337d7733d98288d470502fea38eae
tayvwill <137131113+tayvwill@users.noreply.github.com>
2023-07-06 22:42:16 -0500

    Add files via upload

---
+++ map.js
@@ -0,0 +1,23 @@
+/*const add = (x, y) => x + y;
+const mult = (x,y) => x * y;
+
+const funcCalls = new Map();
+funcCalls.set(add, 0);
+funcCalls.set(mult, 0);
+
+funcCalls.set(add, 1);
+funcCalls.set(mult, 9);
+
+const bandWagon = new Set (['1','2','3','4']);
+*/ const hasDuplicate = new Map();
+hasDuplicate.set ([1, 3, 2, 1], true);
+hasDuplicate.set([1, 5, -1, 4], false);
+
+const vowelCount = [
+    [0, 'a'],
+    [1, 'e'],
+    [2, 'i'],
+    [3, 'o'],
+    [4, 'u']
+];
+const vowelMap = new Map(vowelCount);
